7f6516f2292d79908c053145d223c70f3ba699c7
Jacek <jacek.lynx@gmail.com>
2024-08-27 01:30:44 +0200

    Fix error

--- app/src/main/java/com/example/trustwallet/feature/presentation/CommandsScreen.kt
+++ app/src/main/java/com/example/trustwallet/feature/presentation/CommandsScreen.kt
@@ -23,6 +23,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,7 +40,7 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun CommandsScreen() {
     val viewModel = koinViewModel<CommandsViewModel>()
-    val state by viewModel.state.
+    val state by viewModel.state.collectAsState()
     CommandsScreenContent(
         listOf()
     )
